41bbe26955354f546175b4d911bd50a49c8596ae
이준희 <junh0328@naver.com>
2020-11-09 20:56:39 +0900

    page이름 재 지정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import NewsPage from './pages/NewPage';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
   return <Route path="/:category?" component={NewsPage} />;
